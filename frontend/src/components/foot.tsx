a87a4c6c7dfb7717ec414fcf927ae845203746e9
rohan <123110036@nitkkr.ac.in>
2025-02-28 02:56:44 +0530

    dashboard

---
+++ frontend/src/components/foot.tsx
@@ -0,0 +1,12 @@
+const Footer: React.FC = () => {
+    return (
+        <div className="bg-black text-white text-center p-6">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quaerat hic harum nobis vel a quisquam pariatur ab aut error dolorum, mollitia quo sint ducimus autem? Aspernatur modi dolorum earum.
+            </p>
+            <p className="bg-black text-center mt-4">Copyright © All Rights Reserved</p>
+        </div>
+    );
+}
+
+export default Footer;
